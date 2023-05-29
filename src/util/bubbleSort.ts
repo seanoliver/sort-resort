4e4bf4319e04dd74ef2dc7783d6ca187410f3df2
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-29 13:16:23 -0700

    added bubble sort algorithm

---
+++ src/util/bubbleSort.ts
@@ -0,0 +1,27 @@
+interface BubbleSortProps {
+	arr: number[];
+	updateActiveBars: (activeBars: number[]) => void;
+	updateBars: (bars: number[]) => void;
+}
+
+export default async function bubbleSort(
+	arr: number[],
+	updateActiveBars: (activeBars: number[]) => void,
+	updateBars: (bars: number[]) => void
+) {
+	console.log('arr', arr);
+	const newArr = [...arr];
+	for (let i = newArr.length - 1; i > 0; i--) {
+		for (let j = 0; j < i; j++) {
+			updateActiveBars([j, j + 1]);
+			console.log('newArr', newArr);
+			await new Promise(resolve => setTimeout(resolve, 25));
+			if (newArr[j] > newArr[j + 1]) {
+				[newArr[j], newArr[j + 1]] = [newArr[j + 1], newArr[j]];
+			}
+			updateBars(newArr);
+			updateActiveBars([]);
+		}
+	}
+	return newArr;
+}
